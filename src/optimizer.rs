9335a7404b3bab5bd167df43f6d5cd88b9ea5700
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-19 14:28:48 +0200

    Initial SGD.

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,29 +1,50 @@
 use std::fmt::Display;
 
-pub struct Optimizer {
-    pub(crate) learning_rate: f32,
-    momentum: f32,
-    decay: f32,
+pub enum Optimizer {
+    SGD,
 }
 
-impl Display for Optimizer {
+pub struct Function {
+    optimizer: Optimizer,
+
+    pub(crate) learning_rate: f32,
+    pub(crate) momentum: Option<f32>,
+    pub(crate) decay: Option<f32>,
+}
+
+impl Display for Function {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "\t\tlearning_rate: {}\n", self.learning_rate)?;
-        write!(f, "\t\tmomentum: {}\n", self.momentum)?;
-        write!(f, "\t\tdecay: {}\n", self.decay)?;
+        write!(f, "\t\tmomentum: {}\n", self.momentum.unwrap_or(0.0))?;
+        write!(f, "\t\tdecay: {}\n", self.decay.unwrap_or(0.0))?;
         write!(f, "\t)")
     }
 }
 
-impl Optimizer {
-    pub fn create(name: &str, learning_rate: f32) -> Self {
-        match name {
-            "sgd" => Optimizer {
+impl Function {
+    pub fn create(
+        optimizer: Optimizer,
+        learning_rate: f32,
+        momentum: Option<f32>,
+        decay: Option<f32>
+    ) -> Self {
+        match optimizer {
+            Optimizer::SGD => Function {
+                optimizer: Optimizer::SGD,
                 learning_rate,
-                momentum: 0.0,
-                decay: 0.0,
+                momentum,
+                decay,
             },
-            _ => panic!("Invalid optimizer function"),
+        }
+    }
+
+    pub fn update(&self, values: &mut Vec<f32>, gradients: &Vec<f32>) {
+        match self.optimizer {
+            Optimizer::SGD => {
+                values.iter_mut().zip(gradients.iter()).for_each(|(value, gradient)| {
+                    *value -= self.learning_rate * gradient;
+                });
+            }
         }
     }
 }
