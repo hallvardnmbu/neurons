c9d962e61ab3b9afb7d51c74df0dae4dc390b4de
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-19 11:30:11 +0200

    Add `Display`.

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,15 +1,25 @@
-#[derive(Debug)]
+use std::fmt::Display;
+
 pub struct Optimizer {
-    learning_rate: f64,
-    momentum: f64,
-    decay: f64,
+    pub(crate) learning_rate: f32,
+    momentum: f32,
+    decay: f32,
+}
+
+impl Display for Optimizer {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "\t\tlearning_rate: {}\n", self.learning_rate)?;
+        write!(f, "\t\tmomentum: {}\n", self.momentum)?;
+        write!(f, "\t\tdecay: {}\n", self.decay)?;
+        write!(f, "\t)")
+    }
 }
 
 impl Optimizer {
-    pub fn create(name: &str) -> Self {
+    pub fn create(name: &str, learning_rate: f32) -> Self {
         match name {
             "sgd" => Optimizer {
-                learning_rate: 0.01,
+                learning_rate,
                 momentum: 0.0,
                 decay: 0.0,
             },
