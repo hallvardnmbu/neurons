2b846b8029c8764f6db723b9e58a797f320e703a
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-19 11:30:00 +0200

    Add simple `Objective`.

--- src/objective.rs
+++ src/objective.rs
@@ -1,13 +1,40 @@
-#[derive(Debug)]
+use std::fmt::Display;
+
+pub enum Function {
+    MSE,
+}
+
 pub struct Objective {
-    todo: f32,
+    function: Function,
+}
+
+impl Display for Objective {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self.function {
+            Function::MSE => write!(f, "MSE"),
+        }
+    }
 }
 
 impl Objective {
     pub fn create(name: &str) -> Self {
-        match name {
-            "mse" => Objective { todo: 0.0 },
+        match name.to_lowercase().as_str() {
+            "mse" => Objective { function: Function::MSE },
             _ => panic!("Invalid objective function"),
+        }
+    }
+
+    pub fn loss(&self, y: &Vec<f32>, out: &Vec<f32>) -> (f32, Vec<f32>) {
+        match self.function {
+            Function::MSE => {
+                let loss = y.iter().zip(out.iter())
+                    .map(|(y, o)| (y - o).powi(2))
+                    .sum::<f32>() / y.len() as f32;
+                let gradient = y.iter().zip(out.iter())
+                    .map(|(y, o)| 2.0 * (o - y))
+                    .collect();
+                (loss, gradient)
+            },
         }
     }
 }
