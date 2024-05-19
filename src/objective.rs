9da14d89d112d3cb84e5b253c7fee3ffeee25662
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-19 13:04:51 +0200

    Add a few objective functions.

--- src/objective.rs
+++ src/objective.rs
@@ -1,38 +1,125 @@
 use std::fmt::Display;
 
-pub enum Function {
+pub enum Objective {
+    AE,
+    MAE,
     MSE,
+    RMSE,
+    BinaryCrossEntropy,
+    MulticlassCrossEntropy,
 }
 
-pub struct Objective {
-    function: Function,
+pub struct Function {
+    objective: Objective,
 }
 
-impl Display for Objective {
+impl Display for Function {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self.function {
-            Function::MSE => write!(f, "MSE"),
+        match self.objective {
+            Objective::AE => write!(f, "AE"),
+            Objective::MAE => write!(f, "MAE"),
+            Objective::MSE => write!(f, "MSE"),
+            Objective::RMSE => write!(f, "RMSE"),
+            Objective::BinaryCrossEntropy => write!(f, "BinaryCrossEntropy"),
+            Objective::MulticlassCrossEntropy => write!(f, "MulticlassCrossEntropy"),
         }
     }
 }
 
-impl Objective {
-    pub fn create(name: &str) -> Self {
-        match name.to_lowercase().as_str() {
-            "mse" => Objective { function: Function::MSE },
-            _ => panic!("Invalid objective function"),
+impl Function {
+    pub fn create(objective: Objective) -> Self {
+        match objective {
+            Objective::AE => Function { objective: Objective::AE },
+            Objective::MAE => Function { objective: Objective::MAE },
+            Objective::MSE => Function { objective: Objective::MSE },
+            Objective::RMSE => Function { objective: Objective::RMSE },
+            Objective::BinaryCrossEntropy => Function { objective: Objective::BinaryCrossEntropy },
+            Objective::MulticlassCrossEntropy => Function { objective: Objective::MulticlassCrossEntropy },
         }
     }
 
     pub fn loss(&self, y: &Vec<f32>, out: &Vec<f32>) -> (f32, Vec<f32>) {
-        match self.function {
-            Function::MSE => {
-                let loss = y.iter().zip(out.iter())
-                    .map(|(y, o)| (y - o).powi(2))
+        match self.objective {
+            Objective::AE => {
+                let loss: f32 = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)| (actual - predicted).abs() )
+                    .sum::<f32>();
+                let gradient: Vec<f32> = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)|
+                        if actual == predicted {
+                            0.0
+                        } else if actual > predicted {
+                            1.0
+                        } else {
+                            -1.0
+                        }
+                    ).collect();
+                (loss, gradient)
+            },
+            Objective::MAE => {
+                let loss: f32 = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)| (actual - predicted).abs())
                     .sum::<f32>() / y.len() as f32;
-                let gradient = y.iter().zip(out.iter())
-                    .map(|(y, o)| 2.0 * (o - y))
+                let gradient: Vec<f32> = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)|
+                        if actual == predicted {
+                            0.0
+                        } else if actual > predicted {
+                            1.0
+                        } else {
+                            -1.0
+                        }
+                    ).collect();
+                (loss, gradient)
+            },
+            Objective::MSE => {
+                let loss: f32 = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)| (actual - predicted).powi(2))
+                    .sum::<f32>() / y.len() as f32;
+                let gradient: Vec<f32> = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)| 2.0 * (actual - predicted) / y.len() as f32)
                     .collect();
+                (loss, gradient)
+            },
+            Objective::RMSE => {
+                let loss: f32 = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)| (actual - predicted).powi(2))
+                    .sum::<f32>().sqrt() / y.len() as f32;
+                let gradient: Vec<f32> = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)|
+                        if actual == predicted {
+                            0.0
+                        } else {
+                            (actual - predicted) /
+                                ((actual - predicted).powi(2).sqrt() * y.len() as f32)
+                        }
+                    ).collect();
+                (loss, gradient)
+            },
+            Objective::BinaryCrossEntropy => {
+                let eps: f32 = 1e-7;
+                let loss: f32 = -y.iter().zip(out.iter())
+                    .map(|(actual, predicted)| {
+                            let predicted = predicted.clamp(eps, 1.0 - eps);
+                            (actual * predicted.ln() + (1.0 - actual) * (1.0 - predicted).ln())
+                    }).sum::<f32>() / y.len() as f32;
+                let gradient: Vec<f32> = y.iter().zip(out.iter())
+                    .map(|(actual, predicted)| {
+                        let predicted = predicted.clamp(eps, 1.0 - eps);
+                        -(actual / predicted - (1.0 - actual) / (1.0 - predicted))
+                    }).collect();
+                (loss, gradient)
+            },
+            Objective::MulticlassCrossEntropy => {
+                let eps: f32 = 1e-7;
+                let loss: f32 = -y.iter().zip(out.iter())
+                    .map(|(actual, predicted)|
+                        actual * (predicted + eps).ln()
+                    ).sum::<f32>() / y.len() as f32;
+                let gradient: Vec<f32> = y.iter().zip(out.iter())
+                    .map(|(actual , predicted)|
+                        -actual / (predicted + eps)
+                    ).collect();
                 (loss, gradient)
             },
         }
