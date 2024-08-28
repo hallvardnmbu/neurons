7c067af456068b7ffbbcd183e8fcb7f117e95085
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-08-28 10:00:39 +0200

    Fix typo in doctest.

--- src/tensor.rs
+++ src/tensor.rs
@@ -1481,7 +1481,7 @@ pub fn hadamard3d(ten1: &Vec<Vec<Vec<f32>>>, ten2: &Vec<Vec<Vec<f32>>>) -> Vec<V
 /// # Examples
 ///
 /// ```
-/// use neurons::algebra::mul3d_scalar;
+/// use neurons::tensor::mul3d_scalar;
 ///
 /// let tensor = vec![vec![vec![1.0, 2.0], vec![3.0, 4.0]], vec![vec![5.0, 6.0], vec![7.0, 8.0]]];
 /// let scalar = 2.0;
